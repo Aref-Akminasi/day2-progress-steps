9d5d7e911de03c0569d5001e1ba7aa5b2521320f
Aref Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2023-06-22 16:29:33 +0200

    added comments

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const next = document.getElementById('next');
 const circles = document.querySelectorAll('.circle');
 
 let currentActive = 1;
-
+// For the buttons if the highest or lowest value in the array aren't reached they will call the update function
 next.addEventListener('click', () => {
   currentActive++;
   if (currentActive > circles.length) {
@@ -24,7 +24,7 @@ prev.addEventListener('click', () => {
 function update() {
   circles.forEach((circle, idx) => {
     if (idx < currentActive) {
-      circle.classList.add('active');
+      circle.classList.add('active'); // This section of the function will highligh or de-higlight the circles of lower or higher value than the current state
     } else {
       circle.classList.remove('active');
     }
@@ -32,8 +32,9 @@ function update() {
 
   const actives = document.querySelectorAll('.active');
   progress.style.width =
-    ((actives.length - 1) / (circles.length - 1)) * 100 + '%';
+    ((actives.length - 1) / (circles.length - 1)) * 100 + '%'; // Calculating the number of steps and based on that the percentage
 
+  // For input sanitization, the buttons 'next' and 'prev' will get disabled when the highest or lowest values are reached
   if (currentActive === 1) {
     prev.disabled = true;
   } else if (currentActive === circles.length) {
